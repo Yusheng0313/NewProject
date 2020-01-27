0bffe48844994a279f522bbade47d021a4aae164
Yusheng0313 <492207670@qq.com>
2020-01-28 01:10:13 +0800

    yihao.he

--- 163/csv_day.py
+++ 163/csv_day.py
@@ -7,12 +7,16 @@ import pandas as pd
 
 def download_stock(i_stock_code):
 
+    error_list = ['1111111'] # ['000423', '000515', '000520','000522', '000525', '000538', '000547', '000549', '000570']
+    if i_stock_code in error_list:
+        return 'error'
+
     start_date = '20000101'  # 开始日期
     end_date = '20300101'  # 结束日期
     directory = 'D:\\stock\\StockData\\163\day\\'  # 下载存放目录
 
     HS = '2'                            # 沪深标识 沪0，深1
-    if i_stock_code[0:1] == 6:
+    if i_stock_code[0:1] == '6':
         HS = '0'
     else:
         HS = '1'
@@ -27,14 +31,14 @@ def download_stock(i_stock_code):
 
     req = requests.get(json_url)
 
-    with open(path, 'w', newline='') as f:
+    with open(path, 'w', encoding='utf8', newline='') as f:
         f.write(req.text)
     # file_requests = req.json()
 
-    print('ok')
+    return 'ok'
 
 csv_data =pd.read_csv('D:\\stock\\StockData\\StockList.csv', header=0, encoding="gbk", converters={u'代码': str})  # 读取训练数据
 
-for i in range(0, 5):        # len(csv_data)
+for i in range(0+2257, len(csv_data)):        # len(csv_data)
     print(csv_data.loc[i]['代码'])
-    download_stock(csv_data.loc[i]['代码'])
+    print(download_stock(csv_data.loc[i]['代码']))                 # download_stock(csv_data.loc[i]['代码'])
--- new.py
+++ new.py
@@ -1,57 +1,29 @@
-# ！/usr/bin/env python
-# -*- coding:utf-8 -*-
+import psutil
+import time
 
-# str = 'Hello World!'
-# print(str)
+# 获取cpu个数
+print(psutil.cpu_count())  # 获取cpu 逻辑个数
+print(psutil.cpu_count(logical=False)) # 获取cpu 物理个数
 
-import turtle
-import math
+# 获取cpu利用率
+print(psutil.cpu_percent()) # 返回cpu利用率，如2.1
+print(psutil.cpu_percent(percpu=True)) #返回每个cpu的利用率[3.4, 1.9, 1.7, 1.1, 1.3, 2.1, 1.2, 2.0]
+print(psutil.cpu_percent(interval=2,percpu=True)) #阻塞interval秒，返回这段时间的利用率
 
-t = turtle.Turtle()
+for _ in range(1000):
+  time.sleep(0.01)
+# 以元组的形式，返回cpu的时间花费
+print(psutil.cpu_times())
+"""
+也可以指定 percpu=True
+scputimes(user=16687.609375, system=11913.625000000233, idle=1412197.8906249998, interrupt=1263.90625, dpc=456.09375)
+"""
 
-for i in range(1):
-    t.forward(i * 5)
-    t.right(144)
+# 返回耗时时间比例
+psutil.cpu_times_percent()
 
-sum = 0
-for i in range(1, 100):
-    sum = sum + i*((-1)**(i+1))
-print(sum)
-
-
-while True:
-    num5 = float(input("please input the num:"))
-    a = num5 % 10
-    b = (num5 - a) / 10 % 10
-    c = (num5 - b*10 - a) / 100 % 10
-    d = (num5 - c*100 - b*10 - a) / 1000 % 10
-    e = (num5 - d*1000 - c*100 - b*10 - a) / 10000 % 10
-    if a == e and b == d and 100000 > num5 > 10000:
-        print(a, b, c, d, e)
-        break
-
-x = 1
-while 0 < x < 100:
-
-    if float(math.sqrt(x+100)).is_integer() and float(math.sqrt(x+100+168)).is_integer():
-        print('result：', x)
-        break
-
-    x = x + 1
-
-# 1! + 2! + 3! + … + 20! = 2561327494111820313
-x = 1
-sum = 0
-while x < 21:
-    acc = 1
-    for i in range(1, x+1):
-        acc = acc * i
-    sum = sum + acc
-    x = x + 1
-
-print('result:', x,  acc, sum)
-
-
+# 返回cpu统计信息，包括上下文切换，中断，软中断和系统调用的次数
+psutil.cpu_stats()
 
 
 
